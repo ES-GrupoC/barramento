a425e647f7f38477d4d348c826f0aff9c2f94163
AndreMarques-z3r0x <66215202+AndreMarques-z3r0x@users.noreply.github.com>
2021-12-28 16:10:05 +0000

    Add files via upload

--- pwm.py
+++ pwm.py
@@ -2,7 +2,9 @@ import sys
 import RPi.GPIO as GPIO
 
 #argv[1] = speed »» 5 -> 12 Km/h
-#argv[2] = dir »» 
+#argv[2] = dir »» 1-esquerda 0-nao faz nada 2->direita
+
+#print(str(sys.argv))
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(33,GPIO.OUT)
@@ -14,15 +16,29 @@ my_pwm_dir.start(0)
 
 
 def update_speed(speed):
-    duty = int(speed*14.286 -71.429)
-    print('dutycicle for speed: ', duty)
+    duty = int(int(speed)*8.5714 - 17.857)
+    #print('dutycicle for speed: ', duty)
     my_pwm_speed.ChangeDutyCycle(duty)
     return 0
 
-update_speed(sys.argv[1])
-
 def update_dir(direction):
-    #duty = ....
+    '''
+    full foward -> direita
+    full reverse -> esquerda
+    '''
+    if int(direction) == 1: 
+        duty = 25
+    else:
+        duty = 85
     #print('dutycicle for dir: ', duty)
     #my_pwm_dir.ChangeDutyCycle(duty)
-    #return 0
+    return 0
+
+
+if int(sys.argv[2]) == 0:
+    update_speed(sys.argv[1])
+
+if int(sys.argv[2]) > 0:
+    update_dir(sys.argv[2])
+
+
